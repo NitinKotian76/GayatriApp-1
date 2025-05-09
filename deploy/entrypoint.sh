ecc2bad2ba4af7bf5c2aeb254733cf9b50f0c0b4
NixonNelson22 <nixonnelson22@gmail.com>
2025-05-09 18:38:17 +0530

    docker files updated

--- deploy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
- # poetry run python manage.py collectstatic --no-input
- # poetry run gunicorn main.wsgi:application --bind 0.0.0.0:8000 &
- # poetry run python3 manage.py runserver
-
-# wait
