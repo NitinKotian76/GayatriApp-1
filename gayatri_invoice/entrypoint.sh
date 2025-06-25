a6533892ce1b751c29cfa76556f2875ade8953b6
NixonNelson22 <nixonnelson22@gmail.com>
2025-06-25 12:48:05 +0530

    configured some ci cd configs

--- gayatri_invoice/entrypoint.sh
+++ gayatri_invoice/entrypoint.sh
@@ -1,6 +1,11 @@
-#! /bin/bash
-python manage.py collectstatic --no-input
-gunicorn main.wsgi:application --bind 0.0.0.0:8000 &
-# poetry run python3 manage.py runserver
+#!/bin/bash
+set -e
 
-wait
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+exec gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
