1ef02ce3e9b800bc0a752e5361c36bef0b9b0a77
NixonNelson22 <nixonnelson22@gmail.com>
2026-01-05 18:03:22 +0530

    management command giving undue problems divering to manual superuser creation

--- gayatri_invoice/deploy.sh
+++ gayatri_invoice/deploy.sh
@@ -5,10 +5,10 @@ set -e
 cd /gayatri/gayatriapp
 # Apply database migrations
 python manage.py migrate --noinput
+python manage.py migrate axes --noinput
 
 # Collect static files
 python manage.py collectstatic --noinput
-python manage.py init_app
 
 # Start Gunicorn
 exec gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
